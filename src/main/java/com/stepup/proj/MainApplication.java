2fcd15783c58244b216db28c83c70920f66e873a
Dorofeeva Elena <Doro.Lena.Success@yandex.ru>
2024-09-03 17:22:18 +0300

    Улучшено логика Main

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -2,8 +2,8 @@ package com.stepup.proj;
 
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("1");
         System.out.println("10001");
-        System.out.println("454");
+        System.out.println("451");
     }
 }
